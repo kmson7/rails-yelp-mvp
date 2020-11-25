40f6c12e1edce25a3f9e29d7485bdddfb22ca2a9
Keith Shepherdson <kmson7@gmail.com>
2020-11-25 16:31:40 +0800

    done, no design thought

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,23 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    # raise
+    restaurant = Restaurant.find(params[:restaurant_id])
+    review = Review.new(review_params)
+    # raise
+    review.restaurant = restaurant
+    review.save
+
+    redirect_to restaurant_path(restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
